a03dd99e1571809ddac473c5a9dab09b028efffa
Yelina <98645963+YeFeiFei7@users.noreply.github.com>
2023-07-21 17:37:42 +0600

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,13 +1,33 @@
-package sorts;
+package stack_queue;
+
 
 public class Test {
-    public static int[] generateRandomArray(int maxSize, int maxValue) {
-        int[] arr = new int[(int) (Math.random() * (maxSize) + 1)];
+    public static void main(String[] args) {
+        System.out.println("===Test for Stack===");
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) ((maxValue) * Math.random());
+        System.out.println("size: " + stack.getSize());
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
         }
+        System.out.println("\nsize: " + stack.getSize());
 
-        return arr;
+
+        System.out.println("\n===Test for Queue===");
+        MyQueue queue = new MyQueue();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+
+        System.out.println("size: " + queue.getSize());
+        while(!queue.isEmpty()) {
+            System.out.print(queue.poll() + " ");
+        }
+        System.out.println("\nsize: " + queue.getSize());
     }
 }
